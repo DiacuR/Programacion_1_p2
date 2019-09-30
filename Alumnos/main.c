cdbb2e1d1b84eacf79a70b53044794831aa61ce5
DiacuR <47385825+DiacuR@users.noreply.github.com>
2019-09-30 12:22:08 -0300

    Add files via upload

---
+++ Alumnos/main.c
@@ -0,0 +1,114 @@
+#include "prototipos.h"
+#define MAX 10
+#define MAXL 3
+ ///typedef define tipos de dato.
+
+
+
+
+
+int main()
+{
+
+
+    eAlumno listadoDeAlumnos[MAX];
+    eLocalidad listadoDeLocalidades[MAXL];
+    eContador listaDeCont[MAXL];
+
+
+    inicializarAlumnos(listadoDeAlumnos,MAX,LIBRE);
+
+    int opcion;
+
+    do
+    {
+        printf("\n1. Cargar Alumno.\n2. Cargar 5 alumnos predeterminados.\n3. Dar de baja un alumno");
+        printf("\n4. Buscar alumno.\n5. Listar alumnos.\n6. Modificar \n7. Hardcodear Localidades");
+        printf("\n8. Salir");
+        printf("\nIngrese una opcion: ");
+        scanf("%d",&opcion);
+        switch(opcion)
+        {
+            case 1:
+                cargarAlumnos(listadoDeAlumnos,MAX);
+                break;
+            case 2:
+                hardcodearAlumno(listadoDeAlumnos,5);
+                break;
+            case 3:
+                borrarAlumno(listadoDeAlumnos,MAX,listadoDeLocalidades,MAXL);
+                break;
+            case 4:
+                buscarAlumno(listadoDeAlumnos,MAX);
+                break;
+            case 5:
+                listarAlumnosConLocalidad(listadoDeAlumnos,MAX,listadoDeLocalidades,MAXL);
+                printf("\n");
+                system("pause");
+                break;
+            case 6:
+                modificarAlumno(listadoDeAlumnos,MAX,listadoDeLocalidades,MAXL);
+                break;
+            case 7:
+                inicializarContador(listaDeCont,MAXL);
+                hardcodearLocalidad(listadoDeLocalidades,MAXL);
+                hardcodearContador(listaDeCont,MAXL);
+                break;
+
+            case 8:
+                buscarCantAlumnosPorLocalidad(listadoDeAlumnos,MAX,listadoDeLocalidades,MAXL,listaDeCont,MAXL);
+                break;
+
+            case 9: printf("\nSaliendo..");
+                break;
+            default:
+                printf("Opcion incorrecta");
+
+        }
+
+
+    }while(opcion!=9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+   scanf("%d",&unAlumno.nota);
+   printf("\n El numero es: %d",unAlumno.nota);
+   scanf("%s",&unAlumno.nombre);
+   printf("\n El nombre es: %s",unAlumno.nombre);
+   ///otro alumno
+    scanf("%d",&otroAlumno.nota);
+    otroAlumno=unAlumno;
+   printf("\n El numero es: %d",otroAlumno.nota);
+   scanf("%s",&otroAlumno.nombre);
+   printf("\n El nombre es: %s",otroAlumno.nombre);
+   ///despues de asignar
+ otroAlumno.nota=999;
+ printf("\n El anterior es: %d",unAlumno.nota);
+ printf("\n El actual es: %d",otroAlumno.nota);
+*/
+
+
+
+
+    return 0;
+}
+
+
+
+
---
+++ Alumnos/prototipos.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define LIBRE 0
+#define OCUPADO 1
+
+typedef struct{
+    int idLocalidad;
+    int cantALumnos;
+}eContador;
+
+typedef struct{
+   char nombre[50];
+   int nota;
+   int legajo;
+   int estaVacio;// 0 para esta vacio, 1 para no esta vacio
+   int idLocalidad;
+}eAlumno;
+
+typedef struct{
+int id;
+char localidad[30];
+int codigoPostal;
+}eLocalidad;
+
+void inicializarAlumnos(eAlumno[],int cantidad,int estado);
+void hardcodearAlumno(eAlumno[],int cantidad);
+int listarAlumnosConLocalidad(eAlumno listaDeAlumnos[],int cantidad,eLocalidad listaDeLocalidades[],int tl);
+void mostrarAlumno(eAlumno,eLocalidad);
+int dameIndiceLibre(eAlumno[],int cantidad);
+int cargarAlumnos (eAlumno listaDeAlumnos[],int cantidad);
+void pedirString (char mensaje[],char contenido[],char mensajeDeError[]);
+int pedirEntero (char mensaje[]);
+int buscarAlumno(eAlumno[],int cantidad);
+int borrarAlumno(eAlumno[],int cantidad, eLocalidad[],int tl);
+eAlumno cargarAlumno (void);
+int modificarAlumno(eAlumno[],int cantidad,eLocalidad listaDeLocalidades[],int tl);
+int buscarLocalidadPorId(eLocalidad listaDeLocalidades[],int tl,int idLocalidad);
+
+int buscarCantAlumnosPorLocalidad(eAlumno listadoAlum[], int tamAlum, eLocalidad listadoLoc[], int tamLoc, eContador contador[], int tamCont);
+int hardcodearContador(eContador contador[], int tamCont);
+void inicializarContador(eContador contador[],int tamCont);
+void mostrarTodos(eAlumno alumnos[], int tamAlum, eLocalidad localidad[], int tamLoc);
